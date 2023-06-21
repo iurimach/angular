15b61f9af1ebbd7e0577b3da4c426ca4c79c0ec0
iurimach <119754002+iurimach@users.noreply.github.com>
2023-06-21 19:00:42 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MtavariComponent } from './mtavari/mtavari.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MtavariComponent
   ],
   imports: [
     BrowserModule
